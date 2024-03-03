469e01879cec993e12eace1d2bfcabd5a184a581
imsoumya18 <soumyadeep184@gmail.com>
2024-03-03 10:56:50 +0530

    upload

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ st.title("E-commerce FAQ 🛒")
 st.markdown("""
 The model is just for demonstration purpose only. It is trained using **only 80 Question - Answer pairs**.
 So, expecting it to answer any question other then used for training with high accuracy is not a good idea.
-You can [have a look at all the 80 questions](https://github.com/imsoumya18/E-commerce_FAQ/blob/main/Ecommerce_FAQs.csv) and
-ask something similar or combined of multiple questions.\n
+You can [have a look at all the 80 questions](https://github.com/imsoumya18/E-commerce_FAQ/blob/main/Ecommerce_FAQs.csv)
+and ask something similar or combined of multiple questions.\n
 e.g. ***"How can I create an account?"*** or,\n
 ***"What payment methods do you accept?"*** or even,\n
 ***"How can I create an account and what payment methods do you accept?"*** , etc.
